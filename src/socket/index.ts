af3a07c50c6c38533b8874d47b4a79d2bd5060e0
Batı Gökovalı <batigokovali@icloud.com>
2023-04-18 17:54:48 +0300

    Update

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -6,25 +6,22 @@ import socketioJwt from 'socketio-jwt';
 import jwt from "jsonwebtoken"
 import { JWTTokenAuth } from "../lib/auth/jwt";
 
-
 let onlineUserList: User[] = [];
 
-
- export const authMiddleware = socketioJwt.authorize({
-  secret: process.env.JWT_SECRET as string, 
+export const authMiddleware = socketioJwt.authorize({
+  secret: process.env.JWT_SECRET as string,
   handshake: true,
-  decodedPropertyName: "accessToken", 
+  decodedPropertyName: "accessToken",
 })
 
 export const newConnectionHandler = (socket: Socket) => {
-  
+
   console.log(`New userJoined their id is ${socket.id}`);
   socket.emit("Welcome", socket.id);
 
-
   const verifyJwt = (token: string): Promise<any> => {
     return new Promise((resolve, reject) => {
-      jwt.verify(token, process.env.JWT_SECRET as string, (err, decoded) => { 
+      jwt.verify(token, process.env.JWT_SECRET as string, (err, decoded) => {
         if (err) {
           reject(err);
         } else {
@@ -34,26 +31,21 @@ export const newConnectionHandler = (socket: Socket) => {
     });
   };
 
-
-
   socket.on("outgoing-msg", ({ recipients, message }) => {
     if (Array.isArray(recipients)) {
-        recipients.forEach((recipient: Socket) => {
-            let newRecipients = recipients.filter((r: Socket) => r.id !== recipient.id);
-            newRecipients.push(socket.id);
-            console.log(message, recipients);
-            socket.to(recipient.id).emit("incoming-msg", { 
-                recipients: newRecipients,
-                sender: socket.id,
-                message: message
-            });
-            console.log(message);
+      recipients.forEach((recipient: Socket) => {
+        let newRecipients = recipients.filter((r: Socket) => r.id !== recipient.id);
+        newRecipients.push(socket.id);
+        console.log(message, recipients);
+        socket.to(recipient.id).emit("incoming-msg", {
+          recipients: newRecipients,
+          sender: socket.id,
+          message: message
         });
+        console.log(message);
+      });
     }
-})
-
- 
-
+  })
 
   socket.on("disconnect", () => {
     // onlineUserList=onlineUserList.filter(a=>a.socketId!==socket.id)
