08fea47a9f30b8fb8789e97cf967e8c00e0ab892
Sabin789 <87514484+Sabin789@users.noreply.github.com>
2023-04-18 13:21:03 +0100

    chats

--- src/api/chat/index.ts
+++ src/api/chat/index.ts
@@ -4,9 +4,11 @@ import chatModel  from "./model"
 import UsersModel from "../users/model"
 import { JWTTokenAuth } from "../../lib/auth/jwt";
 import { UserRequest } from "../../lib/auth/jwt";
+import { Server, Socket } from "socket.io";
+
 const chatRouter=Express.Router()
 
-
+const io = new Server();
 
 
 
@@ -27,25 +29,53 @@ chatRouter.get("/", JWTTokenAuth, async (req: UserRequest, res, next) => {
   });
   
 
-  chatRouter.post("/",JWTTokenAuth, async (req: UserRequest, res, next) => {
+//   chatRouter.post("/",JWTTokenAuth, async (req: UserRequest, res, next) => {
+//     try {
+//         const chats = await chatModel.find();
+//         const members = chats.map(chat => chat.members);
+//         const currentUser = req.user; 
+//         // const myChats=members.filter(a=>a!==user)
+//         const recipient=req.body.recipient
+//         const chat=members.filter(a=>a!==recipient)
+//        if(chat){
+//         res.send("In Contact")
+//        }else{
+
+//        }
+   
+//     } catch (error) {
+//       next(error);
+//     }
+//   })
+
+
+chatRouter.post("/", JWTTokenAuth, async (req: UserRequest, res, next) => {
     try {
-        const chats = await chatModel.find();
-        const members = chats.map(chat => chat.members);
-        const currentUser = req.user; 
-        // const myChats=members.filter(a=>a!==user)
-        const recipient=req.body.recipient
-        const chat=members.filter(a=>a!==recipient)
-       if(chat){
-       }
-      res.status(200).send(chats)
+
+      const { userId, roomName } = req.body;
+  
+    const user=req.user
+     
+      const userSocket = io.sockets.sockets.get(userId);
+  
+
+      if (!userSocket) {
+        return res.status(400).send("User is not connected");
+      }
+  
+      userSocket.join(roomName);
+      
+      return res.status(200).send("Room created successfully");
     } catch (error) {
       next(error);
     }
-  })
+  });
 
-  chatRouter.get("/:id", async (req, res, next) => {
+  chatRouter.get("/:id",JWTTokenAuth, async (req: UserRequest, res, next) => {
     try {
       const chats= await chatModel.find()
+      const user=req.user
+
       if(chats)
       res.status(200).send(chats)
     } catch (error) {
