4712ccbf477aa343fc5c8ae5a803098d61b8c281
Sabin789 <87514484+Sabin789@users.noreply.github.com>
2023-04-19 22:34:18 +0100

    Added to onlineUserList

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,36 +1,9 @@
 import { Socket } from "socket.io";
 import { User } from "../types";
-<<<<<<< Updated upstream
-=======
 import jwt, { decode } from "jsonwebtoken"
->>>>>>> Stashed changes
 
-import jwt from "jsonwebtoken";
+// import jwt from "jsonwebtoken";
 
-<<<<<<< Updated upstream
-import { JWTTokenAuth } from "../lib/auth/jwt";
-
-let onlineUserList: User[] = [];
-
-export const newConnectionHandler = (socket: Socket) => {
-  console.log(`New user joined their id is ${socket.id}`);
-  socket.emit("welcome", { message: `${socket.id}` });
-
-  socket.on("setUser", (data: { token: string }) => {
-    const { token } = data;
-    console.log("Received token:", token);
-    const secret = process.env.JWT_SECRET as string;
-
-    jwt.verify(token, secret, (err, decoded) => {
-      if (err) {
-        console.log("Token verification failed:", err);
-      } else {
-        console.log("Token verification successful:", decoded);
-      }
-    });
-  });
-
-=======
 
 let onlineUserList:any = [];
 
@@ -58,7 +31,6 @@ console.log(onlineUserList)
     });
   });
   
->>>>>>> Stashed changes
   socket.on("outgoing-msg", ({ recipients, message }) => {
     if (Array.isArray(recipients)) {
       recipients.forEach((recipient: Socket) => {
@@ -75,11 +47,7 @@ console.log(onlineUserList)
         console.log(message);
       });
     }
-<<<<<<< Updated upstream
-  });
-=======
 })
->>>>>>> Stashed changes
 
   socket.on("disconnect", () => {
     onlineUserList=onlineUserList.filter((a:any)=>a.socketId!==socket.id)
