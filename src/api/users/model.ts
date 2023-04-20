da4ed70d5d28b1bcece4b0e3ae1a1766ca942671
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-04-20 15:59:45 +0300

    default avatar img update

--- src/api/users/model.ts
+++ src/api/users/model.ts
@@ -11,7 +11,8 @@ const UsersSchema = new Schema({
   avatar: {
     type: String,
     required: true,
-    default: "https://static.thenounproject.com/png/5034901-200.png",
+    default:
+      "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png",
   },
   refreshToken: { type: String },
   googleID: { type: String },
